0fa090131e01a0374354b9fb4534701c18e48d65
Shige1299 <example@email.com>
2022-05-16 10:00:28 +0000

    laravel_kadai4

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -3,11 +3,28 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <title>MyNews</title>
     </head>
     <body>
-        <h1>create確認画面です</h1>
+        {{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフ確認用です</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
     </body>
+  
 </html>
