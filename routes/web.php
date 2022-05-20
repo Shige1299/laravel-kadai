e51f059001291d8a55a1f3e0685f77ecba8b5998
Shige1299 <example@email.com>
2022-05-20 08:35:42 +0000

    mynews

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,13 @@ Route::get('XXX','Admin\AAAController@bbb');
 //課題4
 Route::get('admin/profile/create','Admin\ProfileController@add');
 Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
